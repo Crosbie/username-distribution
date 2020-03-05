4df2a2a2bbe4924ee4b30ad41c145822d04d48eb
Eoin Crosbie <ecrosbie@redhat.com>
2020-03-05 09:50:22 +0000

    set port and log

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-app.set('port',process.env.OPENSHIFT_PORT || 8080);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
